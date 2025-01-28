d4efb11abd9e589dd09e0376f415facf7367584c
Mohkaush <mohnish.kaushik@capgemini.com>
2025-01-28 08:29:56 +0000

    conversion operation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+// <-- Number -->
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// <-- Boolean -->
+let isLoggedIn = 1
+// let isLoggedIn = ""
+// let isLoggedIn = "Mohnish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "Mohnish" => true
+
+
+// <-- String -->
+
+let someNumber = 33
+let StringNumber = String(someNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
+
